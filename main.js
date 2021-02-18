401b67c3185619ac9cd44e2cc3e759352fd26c61
Michael Fu Kai Ting <michaelfukai.ting@student.adelaide.edu.au>
2021-02-18 16:49:26 +1030

    getdata from api

--- main.js
+++ main.js
@@ -4,5 +4,17 @@ var app = new Vue({
     data: {
         message: 'NASA MARS ROVER PHOTO',
         newdate:'',
-    }
+        info:[],
+        image:'',
+        
+    },
+
+    mounted(){
+        axios
+        .get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=pZkX9pBMnDGZCJwYkzETQJEyui5dvlMi7VXs5R3T')
+        .then(response => (this.info = response.data.photos))
+        .catch((error => {console.log(error)}))
+    },
+
+
 });
