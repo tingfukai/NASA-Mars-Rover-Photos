6215906452a7e1d731b5ca5437385f54c9707451
Michael Fu Kai Ting <michaelfukai.ting@student.adelaide.edu.au>
2021-02-19 11:47:52 +1030

    a

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 var app = new Vue({
             
     el: '#app',
-    data: {
-        message: 'NASA MARS ROVER PHOTO',
-        newdate:'',
-        info:[],
-        image:'',
+    data(){
+
+        var inputdata = {}
+        inputdata.mydate = '06-03-2015';
         
+        return{
+            message: 'NASA MARS ROVER PHOTO',
+            newdate: inputdata,
+            info:[],
+            image:'',
+        } 
     },
 
     mounted(){
-        axios
-        .get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=pZkX9pBMnDGZCJwYkzETQJEyui5dvlMi7VXs5R3T')
-        .then(response => (this.info = response.data.photos))
-        .catch((error => {console.log(error)}))
+        axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=vzIPsA1gg6ssPjSfCKm9wNwTz0OFjRnbSX4isLbo')
+        .then((response) => {
+        this.info = response.data.photos})
     },
+
+
 
 
 });
