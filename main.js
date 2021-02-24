b66d6653614d9ad7b8c39bc86e620c1d8d4a5cde
Michael Fu Kai Ting <michaelfukai.ting@student.adelaide.edu.au>
2021-02-24 13:49:41 +1030

    Adding comments

--- main.js
+++ main.js
@@ -18,18 +18,20 @@ var app = new Vue({
     },
 
     methods: {
-
+        //When Date changed
         Getdata:function(date,currentpage){
             this.count = 1;
             axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date='+date+ '&page='+ this.count +'&api_key=vzIPsA1gg6ssPjSfCKm9wNwTz0OFjRnbSX4isLbo')
             .then((response) => { 
             this.info = response.data.photos}).then(console.log(this.info))
         },
-
+        //For Previous Button
         PrevPage(date,currentpage)
         {
             this.count = this.count - 1;
             currentpage = currentpage -1;
+
+            //Prevent the user click the previous button when the current page is the first page.
             if(this.count <= 0){
                 alert('This is the first page.')
                 this.count = this.count + 1;
@@ -41,7 +43,7 @@ var app = new Vue({
                 this.info = response.data.photos}).then(console.log(this.info))
             }  
         },
-
+        //For Next Page Button
         NextPage(date,currentpage)
         {
             this.count = this.count + 1;
@@ -50,7 +52,8 @@ var app = new Vue({
             axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date='+this.newdate+ '&page='+ currentpage +'&api_key=vzIPsA1gg6ssPjSfCKm9wNwTz0OFjRnbSX4isLbo')
             .then((response) => { 
             this.info = response.data.photos}).then(console.log(this.info))
-
+            
+            //Check if next page is the last page.
             if(this.info.length < 25 || this.info.length == 0){
                 alert('This is the last page.');
                 this.count = this.count - 1;
