9b4079314a9afd02b70f2d3a7b74c628bc225533
Michael Fu Kai Ting <michaelfukai.ting@student.adelaide.edu.au>
2021-03-01 19:22:20 +1030

    Small changes

--- main.js
+++ main.js
@@ -4,62 +4,68 @@ var app = new Vue({
     data(){
         return{
             Header: 'NASA MARS ROVER PHOTO',
-            newdate: '2021-02-10',
+            newdate: 'YYYY-MM-DD',
+            Today: new Date().toISOString().slice(0,10),
+            shownophoto: false,
             info:[],
-            check:[],
-            count: 1,
+            page: 1,
+            count: 0,
         } 
     },
 
-    mounted() {
-        axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date='+this.newdate+'&page=1&api_key=vzIPsA1gg6ssPjSfCKm9wNwTz0OFjRnbSX4isLbo')
-        .then((response) => { 
-        this.info = response.data.photos}).then(console.log(this.info));
+    //Delay 4sec
+    mounted(){
+
+        setTimeout(() =>{
+            this.shownophoto = true;
+        }, 4000)
+        
     },
 
     methods: {
         //When Date changed
-        Getdata:function(date,currentpage){
-            this.count = 1;
-            axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date='+date+ '&page='+ this.count +'&api_key=vzIPsA1gg6ssPjSfCKm9wNwTz0OFjRnbSX4isLbo')
+        Getdata:function(date){
+
+            this.page = 1
+            axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date='+date+ '&page='+ this.page +'&api_key=vzIPsA1gg6ssPjSfCKm9wNwTz0OFjRnbSX4isLbo')
             .then((response) => { 
             this.info = response.data.photos}).then(console.log(this.info))
+
+            this.count = 1
+
+
         },
         //For Previous Button
-        PrevPage(date,currentpage)
+        PrevPage()
         {
-            this.count = this.count - 1;
-            currentpage = currentpage -1;
+            this.page = this.page - 1
 
             //Prevent the user click the previous button when the current page is the first page.
-            if(this.count <= 0){
+            if(this.page <= 0){
                 alert('This is the first page.')
-                this.count = this.count + 1;
-                currentpage = currentpage +1;
+                this.page = this.page + 1
             }
             else{
-                axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date='+this.newdate+ '&page='+ currentpage +'&api_key=vzIPsA1gg6ssPjSfCKm9wNwTz0OFjRnbSX4isLbo')
+                axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date='+this.newdate+ '&page='+ this.page +'&api_key=vzIPsA1gg6ssPjSfCKm9wNwTz0OFjRnbSX4isLbo')
                 .then((response) => { 
                 this.info = response.data.photos}).then(console.log(this.info))
             }  
         },
         //For Next Page Button
-        NextPage(date,currentpage)
+        NextPage()
         {
-            this.count = this.count + 1;
-            currentpage = currentpage + 1;
+            this.page = this.page + 1
             
-            axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date='+this.newdate+ '&page='+ currentpage +'&api_key=vzIPsA1gg6ssPjSfCKm9wNwTz0OFjRnbSX4isLbo')
+            axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date='+this.newdate+ '&page='+ this.page +'&api_key=vzIPsA1gg6ssPjSfCKm9wNwTz0OFjRnbSX4isLbo')
             .then((response) => { 
             this.info = response.data.photos}).then(console.log(this.info))
             
             //Check if next page is the last page.
             if(this.info.length < 25 || this.info.length == 0){
-                alert('This is the last page.');
-                this.count = this.count - 1;
-                currentpage = currentpage -1;
+                alert('This is the last page.')
+                this.page = this.page - 1
 
-                axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date='+this.newdate+ '&page='+ currentpage +'&api_key=vzIPsA1gg6ssPjSfCKm9wNwTz0OFjRnbSX4isLbo')
+                axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date='+this.newdate+ '&page='+ this.page +'&api_key=vzIPsA1gg6ssPjSfCKm9wNwTz0OFjRnbSX4isLbo')
                 .then((response) => { 
                 this.info = response.data.photos}).then(console.log(this.info))
             }
